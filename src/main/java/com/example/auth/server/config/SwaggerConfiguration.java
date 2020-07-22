c7916ec558766d5a79c5fb238d8f93247c41faef
viren <vincent.renard@gmx.com>
2020-07-22 12:16:26 +0200

    doc not yet

---
+++ src/main/java/com/example/auth/server/config/SwaggerConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.auth.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @autor Vincent
+ * @date 16/07/2020
+ */
+@Configuration
+@EnableSwagger2WebFlux
+public class SwaggerConfiguration {
+}
