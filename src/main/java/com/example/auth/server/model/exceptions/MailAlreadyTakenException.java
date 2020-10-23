506db5254e2fef92e5da252be8fcfc30d30ce7f6
viren <vincent.renard@gmx.com>
2020-10-23 14:45:34 +0200

    FIX bug message in exception + test (http)

--- src/main/java/com/example/auth/server/model/exceptions/MailAlreadyTakenException.java
+++ src/main/java/com/example/auth/server/model/exceptions/MailAlreadyTakenException.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @autor Vincent
  * @date 12/03/2020
  */
-@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Pseudo already taken")
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Mail already taken")
 public class MailAlreadyTakenException extends Exception {
-
 }
